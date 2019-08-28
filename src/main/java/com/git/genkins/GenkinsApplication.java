73f9a911db8521085382a53591d197ab426b96e4
Jagadeesh Pagadala <Jagadeesh.Pagadala@jispl.com>
2019-08-28 20:02:06 +0530

    first commit

---
+++ src/main/java/com/git/genkins/GenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.git.genkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenkinsApplication.class, args);
+	}
+
+}
